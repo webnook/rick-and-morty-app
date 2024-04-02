46c50e81059a9f3d3528158ed5a763a1ae74fe99
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-04-02 20:56:14 +0330

    list of episodes completed includes styling and ...

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,4 +1,5 @@
-import { character } from "../../data/data";
+import { character, episodes } from "../../data/data";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 const CharacterDetail = () => {
   return (
     <div style={{ flex: 1 }}>
@@ -18,11 +19,37 @@ const CharacterDetail = () => {
               className={`status ${
                 character.status === "Dead" ? "red" : ""
               }`}></span>
-                      <span>&nbsp;{ character.status}</span>
+            <span>&nbsp;{character.status}</span>
+            <span> - &nbsp;{character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last Known Location:</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to Favorites</button>
           </div>
         </div>
       </div>
-      <div className="character-episodes"></div>
+      <div className="character-episodes">
+        <div className="title">
+          <h2>List of Episodes</h2>
+          <button>
+            <ArrowUpCircleIcon className="icon" />
+          </button>
+        </div>
+        <ul>
+          {episodes.map((item, index) => (
+            <li key={item.id}>
+              <div>
+                {String(index + 1).padStart(2, "0")} - {item.episode} :&nbsp;
+                <strong>{item.name}</strong>
+              </div>
+              <div className="badge badge--secondary">{item.air_date}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
