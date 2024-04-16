d0aaaf9952c7075c9337df63e445e9016e5715db
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-04-16 20:10:04 +0330

    characters data fetching from API using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { allCharacters } from "../data/data";
-import { useState } from "react";
+// import { allCharacters } from "../data/data";
+import { useEffect, useState } from "react";
 import "./App.css";
 import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const [characters, setCharacters] = useState(allCharacters);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    try {
+      const getData = async () => {
+        const data = await fetch("https://rickandmortyapi.com/api/character");
+        const {results} = await data.json();
+        setCharacters(results.slice(0, 5));
+      };
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <div className="app">
       <Navbar numOfResult={characters.length} />
