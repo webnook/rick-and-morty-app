bc51d6bb4fd61a44d1f0058f0552b235648d4e34
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-04-21 23:44:57 +0330

    loader styles sets in app

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { DNA } from "react-loader-spinner";
 const Loader = () => {
   return (
     <div className="loader">
-      <DNA height="120" width="110" />
+      <DNA height="120" width="120" />
     </div>
   );
 };
