710edb0f9288ab380478a5bd6e62e5eece2a75b8
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-05-29 13:48:18 +0330

    list of episodes created

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { episodes } from "../../data/data";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -8,6 +7,7 @@ import { InfinitySpin } from "react-loader-spinner";
 const CharacterDetail = ({ selectedId }) => {
   const [character, setCharacter] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +17,11 @@ const CharacterDetail = ({ selectedId }) => {
           `https://rickandmortyapi.com/api/character/${selectedId}`
         );
         setCharacter(data);
+        const episodesId = data.episode.map((e) => e.split("/").at(-1));
+        const { data: episodesData } = await axios.get(
+          `https://rickandmortyapi.com/api/episode/${episodesId}`
+        );
+        setEpisodes([episodesData].flat());
       } catch (error) {
         setCharacter(null);
         toast.error(error.response.data.error);
